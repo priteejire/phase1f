18d8add8931b9e09cf6d2073d700788a503b6d4d
priteejire <98096887+priteejire@users.noreply.github.com>
2022-02-18 23:11:21 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,19 @@
+
+public class Array1 {
+	public static void main(String[] args) {
+
+		//single-dimensional array
+		int m[]= {76,24,32,42,54};
+		for(int i=0;i<5;i++) {
+		System.out.println("Elements of array a: "+m[i]);
+		}
+
+
+		//multidimensional array
+		int[][] n = {
+		            {32, 4, 6, 18}, 
+		            {3, 36, 91} };
+		      
+		      System.out.println("\nLength of row 1: " + n[0].length);
+		      }
+		}
